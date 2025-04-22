a52bd31ffdb42e1a878d5d3fef5f323ae315f4c0
jburba <jacob.burba@manchester.ac.uk>
2025-04-22 15:09:38 +0100

    Optional verbose output

--- get_fits_metadata.py
+++ get_fits_metadata.py
@@ -38,9 +38,14 @@ def get_args() -> Namespace:
         default="./",
         help="Output directory for metadata files.  Defaults to './'."
     )
+    parser.add_argument(
+        "-v", "--verbose",
+        action="store_true",
+        help="Verbose output."
+    )
     return parser.parse_args()
 
-def get_fits_header_dict(fpath) -> dict:
+def get_fits_header_dict(fpath, verbose=False, indent="\t") -> dict:
     """
     Get required info from a FITS header.
 
@@ -48,6 +53,10 @@ def get_fits_header_dict(fpath) -> dict:
     ----------
     fpath : Path or str
         Path to a FITS file.
+    verbose : bool, optional
+        Print verbose output.  Defaults to False.
+    indent : str, optional
+        Indentation for verbose output.  Defaults to '\t'.
 
     Returns
     -------
@@ -62,6 +71,13 @@ def get_fits_header_dict(fpath) -> dict:
     header_dict = {}
     with fits.open(fpath) as hdul:
         if len(hdul) > 1:
+            if verbose:
+                print(
+                    indent
+                    + f"More than one HDU found.  "
+                    + f"Trying to find the relevant HDU... ",
+                    end=""
+                )
             # Multiple header data units
             # Try and find the header with the details we want
             naxis_vals = []
@@ -73,6 +89,8 @@ def get_fits_header_dict(fpath) -> dict:
             header_ind = np.where(np.array(naxis_vals) > 0)[0]
             assert header_ind.size > 0, "No valid header found."
             header_ind = header_ind[0]
+            if verbose:
+                print(f"(HDU index {header_ind})")
         else:
             header_ind = 0
         header = hdul[header_ind].header
@@ -83,6 +101,8 @@ def get_fits_header_dict(fpath) -> dict:
             pixtype = "none"
 
         if pixtype.lower() == "healpix":
+            if verbose:
+                print(indent + "Extracting HEALPix metadata...")
             hpx_keys = ["NSIDE", "ORDERING", "NPIX", "FIRSTPIX", "LASTPIX"]
             for key in hpx_keys:
                 try:
@@ -91,6 +111,8 @@ def get_fits_header_dict(fpath) -> dict:
                     pass
         else:
             # Assume data are non-healpix and have CTYPEn keywords
+            if verbose:
+                print(indent + "Extracting (RA, Dec) coordinates...")
 
             # FIXME: This assumes that CRPIXn references the center
             # pixel in the image which need not be the case.
@@ -129,13 +151,17 @@ def get_fits_header_dict(fpath) -> dict:
 if __name__ == "__main__":
     args = get_args()
     for fp in args.file_paths:
+        if args.verbose:
+            print(f"Generating metadata for {fp}")
         fp = Path(fp)
         meta_dict = dict(
             namespace=args.namespace,
             name=fp.name,
             lifetime=args.lifetime,
-            **get_fits_header_dict(fp)
+            **get_fits_header_dict(fp, verbose=args.verbose)
         )
         out_path = Path(args.out_dir) / f"{fp.name}.meta"
         with open(out_path, "w") as f:
             json.dump(meta_dict, f)
+        if args.verbose:
+            print(f"Metadata file written to {out_path}", end="\n\n")
